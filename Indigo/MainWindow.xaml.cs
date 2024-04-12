6b1651b0825b4818193b48955a68edc3732eab11
Кирилл Лосев <grosfae@outlook.com>
2024-04-12 14:00:38 +0300

    Second Commit

--- Indigo/MainWindow.xaml.cs
+++ Indigo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Indigo.Components;
+using Indigo.Pages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,85 +29,8 @@ namespace Indigo
         public MainWindow()
         {
             InitializeComponent();
-            Refresh();
+            MainFrame.Navigate(new ListPage());
         }
-        public void Refresh()
-        {
-            IEnumerable<ComputerCase> computerCases = App.DB.ComputerCase.ToList();
-            maxPage = computerCases.Count() / count;
-            computerCases = computerCases.Skip(count * currentPage).Take(count).ToList();
-            LvNames.ItemsSource = computerCases.ToList();
-            GenerateButtons();
-        }
-        private void GenerateButtons()
-        {
-            ForButtons.Children.Clear();
-            for(int i = 1; i <= maxPage; i++)
-            {
-                Button button = new Button();
-                button.Width = 50;
-                button.Height = 50;
-                button.Content = i.ToString();
-                button.Click += ButtonPage_Click;
-                button.Margin = new Thickness(5, 0, 5, 0);
-                ForButtons.Children.Add(button);
-            }
-        }
-        private void ButtonPage_Click(object sender, RoutedEventArgs e)
-        {
-            currentPage = int.Parse((sender as Button).Content.ToString()) - 1;
-            fakePage = currentPage + 1;
-            Refresh();
-        }
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (fakePage < maxPage)
-            {
-                currentPage++;
-                fakePage = currentPage + 1;
-                Refresh();
-            }
-            else
-            {
-                return;
-            }
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            if(fakePage == 0)
-            {
-                return;
-            }
-            else
-            {
-                currentPage--;
-                fakePage = currentPage + 1;
-                Refresh();
-            }
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            if (fakePage < maxPage)
-            {
-                currentPage = maxPage - 1;
-                fakePage = currentPage + 1;
-                Refresh();
-            }
-            else
-            {
-                return;
-            }
-        }
-
-        private void Button_Click_3(object sender, RoutedEventArgs e)
-        {
-
-                currentPage = 0;
-                fakePage = currentPage + 1;
-                Refresh();
-
-        }
+        
     }
 }
---
+++ Indigo/Pages/EditPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Indigo.Components;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Indigo.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для EditPage.xaml
+    /// </summary>
+    public partial class EditPage : Page
+    {
+        ComputerCase contextCase;
+        public EditPage(ComputerCase computerCase)
+        {
+            InitializeComponent();
+            this.contextCase = computerCase;
+            DataContext = contextCase;
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditPhotoBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog()
+            {
+                Filter = "*.jpg|*.jpg"
+            };
+            if (dialog.ShowDialog().GetValueOrDefault())
+            {
+                contextCase.MainImage = File.ReadAllBytes(dialog.FileName);
+                DataContext = null;
+                DataContext = contextCase;
+            }
+
+        }
+    }
+}
